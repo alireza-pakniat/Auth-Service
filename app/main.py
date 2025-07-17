9786fe796679c8d72ed5ee34f36fdfa2d8bff9b4
Ali Yazdi <aliyazdihasan@gmail.com>
2025-07-17 18:55:52 +0330

    Add Prometheus instrumentation to the FastAPI application

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from app.auth import router as auth_router
 from fastapi.staticfiles import StaticFiles
 from app.database import init_db
 from fastapi.responses import RedirectResponse
+from prometheus_fastapi_instrumentator import Instrumentator
 
 
 app = FastAPI()
@@ -17,3 +18,5 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/")
 def root():
     return RedirectResponse(url="/static/auth.html")
+
+Instrumentator().instrument(app).expose(app)
