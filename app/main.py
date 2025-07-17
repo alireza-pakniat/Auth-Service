c594440b7ba11df4f59aa4174a25f7538dac3b34
Ali Yazdi <aliyazdihasan@gmail.com>
2025-07-17 12:13:24 +0330

    minimal front

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from app.auth import router as auth_router
+from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI()
 
 # ثبت روت احراز هویت
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
